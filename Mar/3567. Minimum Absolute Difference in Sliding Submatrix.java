b1b6ef22a30d56397454b3cabe981b78d5017062
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2026-03-20 21:41:29 +0530

    Create 3567. Minimum Absolute Difference in Sliding Submatrix.java

---
+++ Mar/3567. Minimum Absolute Difference in Sliding Submatrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] minAbsDiff(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans[][] = new int[m-k+1][n-k+1];
+        for(int i=0;i<m-k+1;i++){
+            for(int j=0;j<n-k+1;j++){
+                
+                TreeSet<Integer> set = new TreeSet<>();
+                for(int x=i;x<i+k;x++){
+                    for(int y=j;y<j+k;y++){
+                      set.add(grid[x][y]);
+                    }
+                }
+                if(set.size()==1)continue;
+                int minDiff = Integer.MAX_VALUE;
+
+                Integer prev=null;
+                for(int val:set){
+                    if(prev!=null){
+                        minDiff = Math.min(minDiff,val-prev);
+                    }
+                    prev=val;
+                }
+                ans[i][j]=minDiff;
+            }
+        }
+        return ans;
+    }
+}
